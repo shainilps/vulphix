e628f9c36e0f488de573f52c050630888829ed03
shainilps <shainilps.work@gmail.com>
2025-10-18 21:43:24 +0530

    better builds

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
 echo "Creating bin"
-mkdir -p bin 
+mkdir -p bin
 
 echo "Building for linux"
-CGO=0  GOOS=linux GOARCH=amd64 go build -o ./bin/vulphix-linux-amd64
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/vulphix-linux-amd64
 
 echo "Building for Windows"
-CGO=0  GOOS=windows GOARCH=amd64 go build -o ./bin/vulphix-windows-amd64.exe
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags="-s -w" -o ./bin/vulphix-windows-amd64.exe
 
 echo "Building for Mac"
-CGO=0  GOOS=darwin GOARCH=arm64 go build -o ./bin/vulphix-darwin-arm64
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -ldflags="-s -w" -o ./bin/vulphix-darwin-arm64
